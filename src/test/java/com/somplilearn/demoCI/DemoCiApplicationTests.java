42518a75d509a6e108c5b4c1051e067711c68587
student <ujjwals@vmware.com>
2020-11-28 08:44:59 +0000

    First Commit

---
+++ src/test/java/com/somplilearn/demoCI/DemoCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somplilearn.demoCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
